7e677b3cabe933bbec023a7c2f24a0738b0f1d60
venky123895 <devathotivenkataramana@gmail.com>
2023-03-07 09:03:25 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
+import MovieContainer from './components/MovieContainer';
 
-function App() {
+const App = () => {
+
+const [first, setfirst] = useState()
+const[inp,getInp]=useState();
+const[movie,movieDetails]=useState()
+
+useEffect(() => {
+  async function getData(){
+    const streaResponse= await fetch(`https://www.omdbapi.com/?t=${first}&apikey=4038d0e7`);
+    const textResponse= await streaResponse.text();
+    const jsonData=JSON.parse(textResponse);
+    movieDetails(jsonData)
+    // console.log(jsonData)
+    
+  }
+ 
+  getData()
+}, [first])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <div className='header'>
+          HOOKED
+        </div>
+        <div className='inputContainer'>
+          <input type="text" className='inp' onChange={(e)=>{
+              getInp(e.target.value);
+              console.log(inp)
+          }}/>
+          <button className='but' onClick={()=>{
+            setfirst(inp)
+            console.log(first)
+          }} >Search</button>
+        </div>
+        <p style={{textAlign:"center",fontSize:"1.3rem",marginTop:"25px"}}>Sharing a few of our favourite movies</p>
+
+        <div className='movieContainer'>
+        <div className='title'>
+        <p className='movieTitle'>{movie.Title}</p>
+        <img src={movie.Poster} alt='image' className='image'/>
+        <p className='movieYear'>{movie.Year}</p>
     </div>
-  );
+        </div>
+
+     
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MovieContainer.jsx
@@ -0,0 +1,13 @@
+// import React, { useContext } from 'react'
+// import { GlobalData } from '../App'
+// import './movie.css'
+
+// const MovieContainer = () => {
+//     const movie =useContext(GlobalData)
+    
+//   return (
+ 
+//   )
+// }
+
+// export default MovieContainer
